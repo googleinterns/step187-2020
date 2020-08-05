64cc10f061ff2d674bca5c295b1ce42fa48893a5
Catherine Yu <39574893+cyu6@users.noreply.github.com>
2020-08-05 13:57:53 -0400

    Add NavBar component and Home page component (#10)
    * Add NavBar component using Material UI and edit App.js for home page
    
    * Styling fixes
    
    * Add test for Home content and refactor components code
    
    * Style fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import "./App.css";
-
+import React, { Component, Fragment } from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
   // Test to see if backend servlet connects to frontend.
   componentDidMount() {
-    console.log("Inside componentDidMount!");
-    fetch("/blackswan/test")
+    fetch("/api/v1/test-servlet")
       .then((response) => response.text())
       .then((text) => {
         console.log("here is the text from servlet: ", text);
@@ -16,12 +21,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Home isLoggedIn={this.state.isLoggedIn} />
+      </Fragment>
     );
   }
 }
