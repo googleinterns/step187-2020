4bd2c925686b1dbddd3fb49be0218e0c525c8193
Leodson <leodsonj@gmail.com>
2020-08-06 05:25:49 -0700

    Restructures files for alert configuration feature

---
+++ frontend/src/AlertConfiguration/AlertConfiguration.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AlertConfigurationApp from './AlertConfigurationComponents/AlertConfigurationApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AlertConfigurationApp/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
