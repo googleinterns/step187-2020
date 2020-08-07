dc50eccd4a3f1c384b08553158f8776e011b4eaa
Catherine Yu <39574893+cyu6@users.noreply.github.com>
2020-08-07 13:57:59 -0400

    Set up react-router for nav bar (#17)
    * Set up react-router for nav bar
    
    * Fix react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
+import Dummy from './Dummy';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App}>
+        </Route>
+        {/* Dummy is a temporary component to routing works */}
+        <Route path="/alerts" component={Dummy}>
+          {/* TODO: import AlertsManagement */}
+        </Route>
+        <Route path="/configs">
+          {/* TODO: import AlertConfiguration */}
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
