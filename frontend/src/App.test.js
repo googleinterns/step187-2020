8527f2e615782441dc8df03ad4e4390f383a81a1
cyu6 <catyu6000@gmail.com>
2020-08-11 15:30:01 -0400

    Add tests for App.js (forgot these in the last commit)

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
 import { shallow, configure } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
+import { enableFetchMocks } from 'jest-fetch-mock';
+enableFetchMocks();
 import App from './App';
 
 configure({ adapter: new Adapter() });
 
-const LOGIN_STATUS = "logged in\n";
 const LOGIN_URL = "/_ah/login?continue=%2F";
 const LOGIN_MESSAGE = "logged in\n/_ah/logout?continue=%2F";
-const LOGOUT_STATUS = "stranger\n";
 const LOGOUT_URL = "/_ah/logout?continue=%2F";
+const LOGOUT_MESSAGE = "stranger\n/_ah/logout?continue=%2F";
 
 // TODO: write the tests below.
-// describe("getLoginStatus", () => {
-//   it("sets logged in state when user is logged in", () => {
-//   });
+describe("getLoginStatus", () => {
+  const getLoginStatus = jest.spyOn(App.prototype, 'getLoginStatus');
+
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  it("sets logged in state when user is logged in", async () => {
+    fetch.mockResponse(LOGIN_MESSAGE);
+    // getLoginStatus is called in componentDidMount, which is called by shallow.
+    const component = shallow(<App />);
+    component.setState({ isLoggedIn: false, logURL: ""});
+    expect(getLoginStatus).toHaveBeenCalled();
+
+    setTimeout(() => {
+      component.update()
+      expect(component.state('isLoggedIn')).toEqual(true); 
+      expect(component.state('logURL')).toEqual(LOGOUT_URL);
+    }, 3000);
+  });
   
-//   it("sets logged out state when user is logged out", () => {
-//   });
-// });
+  it("sets logged out state when user is logged out", () => {
+    fetch.mockResponse(LOGOUT_MESSAGE);
+    const component = shallow(<App />);
+    component.setState({ isLoggedIn: false, logURL: ""});
+    expect(getLoginStatus).toHaveBeenCalled();
+
+    setTimeout(() => {
+      component.update()
+      expect(component.state('isLoggedIn')).toEqual(false); 
+      expect(component.state('logURL')).toEqual(LOGIN_URL);
+    }, 3000);
+  });
+});
