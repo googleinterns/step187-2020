1de3a3289591c8ab9ad01822e301f080257e55e7
cyu6 <catyu6000@gmail.com>
2020-08-12 12:19:58 -0400

    Fetch alerts from Datastore and display on frontend

--- frontend/src/AlertsManagement/AlertsContent.test.js
+++ frontend/src/AlertsManagement/AlertsContent.test.js
@@ -7,7 +7,7 @@ import { createShallow } from '@material-ui/core/test-utils';
 import { PureAlertsContent as AlertsContent } from "./AlertsContent";
 import AlertsList from "./AlertsList";
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
 
 describe("handleTabs", () => {
   const tabLabels = {
@@ -39,10 +39,27 @@ describe("handleTabs", () => {
 });
 
 describe("handleCheckbox", () => {
-  const editUnchecked = [1, 2, 3];
-  const editChecked = [4, 5, 6, 0];
-  const missingChecked = [4, 5, 6];
-  const doubleChecked = [0, 4, 5, 6];
+  // Mock data for the alerts, unresolved and resolved.
+  const fakeAlerts = new Map();
+  fakeAlerts.set(0, {
+    timestamp: "2020-01-20",
+    anomalies: 3,
+  });
+  fakeAlerts.set(1, {
+    timestamp: "2019-09-19",
+    anomalies: 2,
+  });
+  fakeAlerts.set(2, {
+    timestamp: "2019-08-06",
+    anomalies: 2,
+  });
+  const fakeUnresolved = [0, 1];
+  const fakeResolved = [2];
+
+  // Expected mock data.
+  const editUnchecked = [1];
+  const editChecked = [2, 0];
+  const doubleChecked = [0, 2];
 
   const styles = { 
     root: {
@@ -52,9 +69,22 @@ describe("handleCheckbox", () => {
     }
   };
 
-  it("should maintain the correct unresolved and resolved elements after click", () => {
-    const wrapper = shallow(<AlertsContent classes={styles} />);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(<AlertsContent classes={styles} />);
+    wrapper.setState({ 
+      allAlerts: fakeAlerts,
+      unchecked: fakeUnresolved,
+      checked: fakeResolved,
+    });
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  })
+
+  it("should maintain the correct unresolved and resolved elements after click", () => {
     act(() => {
       const boxes = wrapper.find(AlertsList).at(0).dive().find('WithStyles(ForwardRef(Checkbox))');
       boxes.at(0).simulate('click');
@@ -65,25 +95,25 @@ describe("handleCheckbox", () => {
   });
 
   it("should throw error if alert is considered both unresolved and resolved", () => {
-   const wrapper = shallow(<AlertsContent classes={styles} />);
     wrapper.setState({ checked: doubleChecked });
-    const boxes = wrapper.find(AlertsList).at(0).dive().find('WithStyles(ForwardRef(Checkbox))');
 
+    const boxes = wrapper.find(AlertsList).at(0).dive().find('WithStyles(ForwardRef(Checkbox))');
     function testCheckbox() { boxes.at(0).simulate('click'); }
 
-    expect(testCheckbox).toThrowError('Misplaced alert')
+    expect(testCheckbox).toThrowError('Misplaced alert');
   });
 
   it("should throw error if alert is neither unresolved nor resolved", () => {
-    const wrapper = shallow(<AlertsContent classes={styles} />);
-    wrapper.setState({ checked: missingChecked });
     const boxes = wrapper.find(AlertsList).at(0).dive().find('WithStyles(ForwardRef(Checkbox))');
     wrapper.setState({ unchecked: editUnchecked });
-
     function testCheckbox() { boxes.at(0).simulate('click'); }
 
     expect(testCheckbox).toThrowError('Misplaced alert')
   });
 
   // TODO: write test for sending POST request to servlet
+});
+
+describe("fetch alerts", () => {
+  
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import Dummy from './Dummy';
+import AlertsManagement from './AlertsManagement/AlertsManagement';
 import './index.css';
 
 ReactDOM.render(
@@ -12,10 +13,10 @@ ReactDOM.render(
         <Route exact path="/" component={App}>
         </Route>
         {/* Dummy is a temporary component to routing works */}
-        <Route path="/alerts" component={Dummy}>
+        <Route path="/alerts" component={AlertsManagement}>
           {/* TODO: import AlertsManagement */}
         </Route>
-        <Route path="/configs">
+        <Route path="/configs" component={Dummy}>
           {/* TODO: import AlertConfiguration */}
         </Route>
       </Switch>
