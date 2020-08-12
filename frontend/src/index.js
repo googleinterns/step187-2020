8557a7482402d11fac42ac6fe5dbacc0ba19b628
cyu6 <catyu6000@gmail.com>
2020-08-12 18:02:03 -0400

    Undo index.js edit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import Dummy from './Dummy';
-import AlertsManagement from './AlertsManagement/AlertsManagement';
 import './index.css';
 
 ReactDOM.render(
@@ -13,10 +12,10 @@ ReactDOM.render(
         <Route exact path="/" component={App}>
         </Route>
         {/* Dummy is a temporary component to routing works */}
-        <Route path="/alerts" component={AlertsManagement}>
+        <Route path="/alerts" component={Dummy}>
           {/* TODO: import AlertsManagement */}
         </Route>
-        <Route path="/configs" component={Dummy}>
+        <Route path="/configs" >
           {/* TODO: import AlertConfiguration */}
         </Route>
       </Switch>
