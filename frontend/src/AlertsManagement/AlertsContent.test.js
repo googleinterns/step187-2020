41ab188a0a7385f8ca76d985c4e12e1acc0cc137
cyu6 <catyu6000@gmail.com>
2020-08-17 13:34:23 -0400

    Fix alerts content test in accordance with edits

--- frontend/src/AlertsManagement/AlertsContent.test.js
+++ frontend/src/AlertsManagement/AlertsContent.test.js
@@ -136,12 +136,27 @@ describe("fetch alerts", () => {
   // Expected allAlerts, unchecked, and checked.
   const expectedAlerts = new Map();
   expectedAlerts.set(0, {
-    timestamp: "Sun Dec 08 2019",
-    anomalies: 2,
+    timestampDate: "Sun Dec 08 2019",
+    anomalies: [{
+      dataPoints: new Map([ ["2019-11-24", 79], ]), 
+      dimensionName: "Ramen", metricName: "Interest Over Time",
+      timestampDate: "Sun Dec 29 2019"
+    },
+    { 
+      dataPoints: new Map([ ["2019-10-27", 53] ]), 
+      dimensionName: "Ramen", metricName: "Interest Over Time",
+      timestampDate: "Sun Dec 01 2019",
+    }],
+    status: "UNRESOLVED",
   });
   expectedAlerts.set(1, {
-    timestamp: "Fri Dec 27 2019",
-    anomalies: 1,
+    timestampDate: "Fri Dec 27 2019",
+    anomalies: [{ 
+      dataPoints: new Map([ ["2019-10-24", 46] ]), 
+      dimensionName: "Ramen", metricName: "Interest Over Time",
+      timestampDate: "Wed Nov 27 2019"
+    }],
+    status: "RESOLVED",
   });
   const expectedUnchecked = [0];
   const expectedChecked = [1]
