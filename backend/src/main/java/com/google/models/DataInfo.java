86e7e16845f7886ecff76168da8f28b1ff850aa2
Melody Chen <34081368+melodychn@users.noreply.github.com>
2020-08-24 10:12:46 -0700

    Working related data generator (#42) (#43)
    * working dummy related data generator
    
    * minor fixes
    
    * clarify comments
    
    * fixes to change requested
    
    * chain append strs
    
    * Add clarify comments
    
    * More advance implementation of RelatedDataGenerator (#43)
    
    * more advance related data generator
    
    * minor fixes + small refactor
    
    * extra comment to clarify code
    
    * fix changes requested
    
    * fix merge conflict

---
+++ backend/src/main/java/com/google/models/DataInfo.java
@@ -0,0 +1,72 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.models;
+
+
+/** 
+ * Wrapper for meta data of a related data type used in RelatedDataGenerator.
+ * This is an immutable class.  
+ */
+public final class DataInfo {
+  
+  private final String metricName;
+  private final String dimensionName;
+  private final String username;
+
+  /** TODO: Change username to List to deal with multiple users requesting same related data. */
+  public DataInfo(String metricName, String dimensionName, String username) {
+    this.metricName = metricName;
+    this.dimensionName = dimensionName;
+    this.username = username;
+  }
+
+  public String getMetricName() {
+    return metricName;
+  }
+
+  public String getDimensionName() {
+    return dimensionName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public String toString() {
+    return new StringBuilder()
+        .append("Metric Name: ").append(metricName).append("\n")
+        .append("Dimension Name: ").append(dimensionName).append("\n")
+        .append("Username: ").append(username).append("\n")
+        .toString();
+  }
+
+  @Override
+   public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+
+    if (!(o instanceof DataInfo)) {
+      return false;
+    }
+
+    DataInfo target = (DataInfo) o;
+
+    return target.metricName == metricName &&
+        target.dimensionName == dimensionName;
+  }
+
+}
---
+++ backend/src/main/java/com/google/models/RelatedData.java
@@ -0,0 +1,158 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.models;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.models.MetricValue;
+import com.google.models.Timestamp;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EmbeddedEntity;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.SortedMap;
+import java.time.format.DateTimeParseException;
+
+/** 
+ * Store related data for anomalies with data points and meta data.
+ * This is an immutable class. 
+ */
+public final class RelatedData {
+  public static final String RELATED_DATA_ENTITY_KIND = "relatedData";
+  public static final String USERNAME_PROPERTY = "username";
+  public static final String METRIC_NAME_PROPERTY = "metricName";
+  public static final String DIMENSION_NAME_PROPERTY = "dimensionName";
+  public static final String DATA_POINTS_PROPERTY = "dataPoints";
+
+  private static final String INDENT = "  ";
+  private static final String INDENT_DOUBLE = "    ";
+  private static final String DUMMY_USERNAME = "bob@";
+  private static final String DUMMY_METRIC_NAME = "Sample metric name";
+  private static final String DUMMY_DIMENSION_NAME = "Sample dimension name";
+  private static final Map<Timestamp, MetricValue> DUMMY_DATA_POINTS = ImmutableMap.of( 
+      Timestamp.getDummyTimestamp(1), new MetricValue(1), 
+      Timestamp.getDummyTimestamp(2), new MetricValue(2), 
+      Timestamp.getDummyTimestamp(3), new MetricValue(3));
+  
+  private final String metricName;
+  private final String dimensionName;
+  private final Map<Timestamp, MetricValue> dataPoints;
+  private final String username;
+
+  public RelatedData(String username, String metricName, String dimensionName, 
+      Map<Timestamp, MetricValue> dataPoints) {
+    this.username = username;
+    this.metricName = metricName;
+    this.dimensionName = dimensionName;
+    this.dataPoints = ImmutableMap.copyOf(dataPoints);
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getMetricName() {
+    return metricName;
+  }
+
+  public String getDimensionName() {
+    return dimensionName;
+  }
+
+  public Map<Timestamp, MetricValue> getDataPoints() {
+    return dataPoints;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+
+    if (!(o instanceof RelatedData)) {
+      return false;
+    }
+
+    RelatedData target = (RelatedData) o;
+
+    return target.metricName.equals(metricName)
+        && target.dimensionName.equals(dimensionName) 
+        && target.username.equals(username)
+        && dataPoints.entrySet().stream().allMatch(
+          e -> e.getValue().equals(target.dataPoints.get(e.getKey()))
+        );
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder str = new StringBuilder()
+        .append(INDENT).append("Username: ").append(username).append("\n")
+        .append(INDENT).append("Metric Name: ").append(metricName).append("\n")
+        .append(INDENT).append("Dimension Name: ").append(dimensionName).append("\n")
+        .append(INDENT).append("Datapoints: \n");
+    dataPoints.forEach((key, value) -> 
+      str.append(INDENT_DOUBLE).append(key).append(": ").append(value).append("\n")
+    );
+    return str.toString();
+  }
+
+  /** Return a hard-coded RelatedData used for tests and DummyRelatedDataGenerator. */
+  public static RelatedData getDummyRelatedData() {
+    return new RelatedData(DUMMY_USERNAME, DUMMY_METRIC_NAME, DUMMY_DIMENSION_NAME, 
+        DUMMY_DATA_POINTS);
+  }
+
+  public Entity toEntity() {
+    Entity relatedDataEntity = new Entity(RELATED_DATA_ENTITY_KIND);
+    relatedDataEntity.setProperty(USERNAME_PROPERTY, username);
+    relatedDataEntity.setProperty(METRIC_NAME_PROPERTY, metricName);
+    relatedDataEntity.setProperty(DIMENSION_NAME_PROPERTY, dimensionName);
+
+    EmbeddedEntity dataPointsEntity = new EmbeddedEntity();
+    // Datastore stores numbers as long, so to metricValue should be cast to a long. 
+    dataPoints.forEach((timestamp, metricValue) -> 
+        dataPointsEntity.setProperty(timestamp.toString(), (long) metricValue.getValue()));
+    relatedDataEntity.setProperty(DATA_POINTS_PROPERTY, dataPointsEntity);
+
+    return relatedDataEntity;
+  }
+
+  /** TODO: Move Entity to Embedded Entity conversion somewhere else that all entity can share. */
+  public EmbeddedEntity toEmbeddedEntity() {
+    Entity relatedDataEntity = toEntity();
+    EmbeddedEntity relatedDataEmbeddedEntity = new EmbeddedEntity();
+    relatedDataEmbeddedEntity.setKey(relatedDataEntity.getKey());
+    relatedDataEmbeddedEntity.setPropertiesFrom(relatedDataEntity);
+    
+    return relatedDataEmbeddedEntity;
+  }
+
+  public static RelatedData createFromEmbeddedEntity(EmbeddedEntity relatedDataEmbeddedEntity) {
+    EmbeddedEntity dataPointsEE = (EmbeddedEntity) relatedDataEmbeddedEntity.getProperty(DATA_POINTS_PROPERTY);
+    SortedMap<Timestamp, MetricValue> dataPointsMap = new TreeMap<>();
+
+    if (dataPointsEE != null) {
+      for (String key : dataPointsEE.getProperties().keySet()) {
+        dataPointsMap.put(new Timestamp(key), new MetricValue(((long) dataPointsEE.getProperty(key))));
+      }
+    }
+
+    return new RelatedData((String) relatedDataEmbeddedEntity.getProperty(USERNAME_PROPERTY), 
+        (String) relatedDataEmbeddedEntity.getProperty(METRIC_NAME_PROPERTY),
+        (String) relatedDataEmbeddedEntity.getProperty(DIMENSION_NAME_PROPERTY),
+        dataPointsMap);
+  }
+
+}
