45d9c38d5c04785350a06e4ea8569344fff6fec7
Leodson <leodsonj@gmail.com>
2020-08-28 03:56:56 -0700

    Creates a configuration model

---
+++ backend/src/main/java/com/google/models/Configuration.java
@@ -0,0 +1,23 @@
+package com.google.models;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EmbeddedEntity;
+import com.google.common.collect.ImmutableList;
+import com.google.models.Anomaly;
+import com.google.models.Timestamp;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.OptionalLong;
+
+public final class Configuration {
+  private final String dimension;
+  private final String metric;
+  private final String relatedDimension;
+  private final String relatedMetric;
+
+  private Configuration(String dimension, String metric, String relatedDimension, String relatedMetric) {
+    this.dimension = dimension;
+    this.metric = metric;
+    this.relatedDimension = relatedDimension;
+    this.relatedMetric = relatedMetric;
+  }
+}
