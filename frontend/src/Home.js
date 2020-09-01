cc615ddffb1b481c3baf127736631a07c86299fc
cyu6 <catyu6000@gmail.com>
2020-09-01 18:58:21 -0400

    Revert file name

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,8 +3,6 @@ import './Home.css';
 import { getLoginStatus } from './login_helper';
 import Typography from '@material-ui/core/Typography';
 
-const LOGO_LOCATION = './greyswan.png';
-
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -21,7 +19,7 @@ export default function Home() {
       <Typography variant="h2" style={{ margin: '30px'}}>
         Welcome to GreySwan!  
       </Typography>
-      <img src={require(LOGO_LOCATION)} alt="Grey swan" height="100" width="100" style={{ marginLeft: '10px'}} />
+      <img src={require('./greyswan.png')} alt="Grey swan" height="100" width="100" style={{ marginLeft: '10px'}} />
       <Typography variant="h4" className="message" style={{ margin: '30px' }}>
         {isLoggedIn ? "Thank you for visiting our app!" :  "Please log in."}
       </Typography>
